aaa25534bd78cab1a3caa1ee192a114f93379976
Brian McFadden <101163132+bmcfads@users.noreply.github.com>
2024-07-28 12:36:30 -0700

    Update comments

--- recipes/STOP/direct/train.py
+++ recipes/STOP/direct/train.py
@@ -6,27 +6,27 @@ import speechbrain as sb
 from speechbrain.utils.distributed import run_on_main
 
 if __name__ == "__main__":
-    # Load hyperparameters file with command-line overrides
+    # Load hyperparameters file with command-line overrides.
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
 
-    show_results_every = 100  # Plots results every N iterations
+    show_results_every = 100  # Plots results every N iterations.
 
-    # Create ddp_group with the right communication protocol
+    # Create ddp_group with the right communication protocol.
     sb.utils.distributed.ddp_init_group(run_opts)
 
-    # Create experiment directory
+    # Create experiment directory.
     sb.create_experiment_directory(
         experiment_directory=hparams["output_folder"],
         hyperparams_to_save=hparams_file,
         overrides=overrides,
     )
 
-    # Dataset prep (parsing STOP)
+    # Dataset prep (parsing STOP).
     from prepare import prepare_STOP  # noqa
 
-    # Multi-GPU (DDP) save data preparation
+    # Multi-GPU (DDP) save data preparation.
     run_on_main(
         prepare_STOP,
         kwargs={
